44e008f01f1a4c5ebb1c1730fe2ecdaab0a6a2ee
Akrimi26 <akrimideveloper@gmail.com>
2026-05-07 19:13:16 +0100

    add homepage redirect

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
 from django.shortcuts import redirect
 from .views import demandes, update_statut, dashboard , form_page, mes_demandes_page,admin_demandes_page, login_page, dashboard_page, suivi_page, suivi_demande
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('demandes/', demandes),
@@ -18,5 +21,5 @@ urlpatterns = [
     path('suivi/', suivi_demande),
     path('suivi-page/', suivi_page),
     path('admin-demandes/', admin_demandes_page),
-     path('', lambda request: redirect('/api/form/')),
+    path('', lambda request: redirect('/api/form/')),
 ]
