745948ba1a3fc9785527e977467199471be5e2ab
Akrimi26 <akrimideveloper@gmail.com>
2026-05-07 18:23:43 +0100

    fix static root

--- baladia1/settings.py
+++ baladia1/settings.py
@@ -135,3 +135,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
