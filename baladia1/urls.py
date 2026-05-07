bf7c39b8eebe70f64b79ddfe69e9ab3e41bbebd1
Akrimi26 <akrimideveloper@gmail.com>
2026-05-07 19:22:27 +0100

    add homepage redirect

--- baladia1/urls.py
+++ baladia1/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.shortcuts import redirect
 urlpatterns = [
+    path('', lambda request: redirect('/api/form/')),
     path('admin/', admin.site.urls),
     path('api/', include('services.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
